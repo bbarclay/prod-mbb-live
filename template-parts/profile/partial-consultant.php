76a76fefb4c06c7c8cf698699865b6133e81089b
julycabigas <july@businessblueprint.com>
2018-07-13 12:12:39 +0800

    change consultant page

--- template-parts/profile/partial-consultant.php
+++ template-parts/profile/partial-consultant.php
@@ -1,54 +1,56 @@
 <div class="upt-consultant">
 
-	<?php if( have_rows('consultants') ) :
 
-			echo $consultant = do_shortcode('[mbb_your_consultant]'); 
+	<?php 
+			$consultant = do_shortcode('[mbb_your_consultant]'); 
 
-			while( have_rows('consultants') ) : the_row();   
-	
-					if( $consultant == get_sub_field('name') ) : ?>
+			if( have_rows('consultants') ) :
 
-
-						<div class="info">
-							<div class="info-left">
-								 
-								 <img src="<?php echo get_sub_field('photo') ?>" width="100%" alt="Business Blueprint Consultant" />
-
-								 <?php 
-
-								  	echo '<div class="name">' . get_sub_field('name') . '</div>';
-
-								 	if( have_rows('buttons') ) :
-								 		$count = 0;
-								 		echo '<ul class="list">';
-								 		while( have_rows('buttons') ) : the_row(); $count++; ?>
-
-								 			<li><a href="<?php echo ($count == 1) ? 'mailto:' : '' ?><?php echo get_sub_field('link') ?>" class="btn" target="_blank"><?php echo get_sub_field('text') ?></a></li>
-
-								 <?php  endwhile; 
-								 		echo '</ul>';
-								 	endif;
-								 ?>	
-
-							 </div>
-
-							 <?php if( get_sub_field('bio') ) { ?>
-								 <div class="bio info-right">
-								 	<?php echo get_sub_field('bio') ?>
-								 </div>
-							 <?php } ?>	 
-
-						</div>
+			
+					while( have_rows('consultants') ) : the_row();   
+			
+							if( $consultant == get_sub_field('name') ) : ?>
 
 
-			<?php  else: ?>
-					
-					<p>N/A</p>
-						
-				<?php
-				endif;
-			endwhile; 
+								<div class="info">
+									<div class="info-left">
+										 
+										 <img src="<?php echo get_sub_field('photo') ?>" width="100%" alt="Business Blueprint Consultant" />
 
-	      endif; ?>	
+										 <?php 
+
+										  	echo '<div class="name">' . get_sub_field('name') . '</div>';
+
+										 	if( have_rows('buttons') ) :
+										 		$count = 0;
+										 		echo '<ul class="list">';
+										 		while( have_rows('buttons') ) : the_row(); $count++; ?>
+
+										 			<li><a href="<?php echo ($count == 1) ? 'mailto:' : '' ?><?php echo get_sub_field('link') ?>" class="btn" target="_blank"><?php echo get_sub_field('text') ?></a></li>
+
+										 <?php  endwhile; 
+										 		echo '</ul>';
+										 	endif;
+										 ?>	
+
+									 </div>
+
+									 <?php if( get_sub_field('bio') ) { ?>
+										 <div class="bio info-right">
+										 	<?php echo get_sub_field('bio') ?>
+										 </div>
+									 <?php } ?>	 
+
+								</div>
+
+								
+							<?php
+							endif;
+					endwhile; ?>
+	<?php   else: ?>
+							
+			  <p>N/A</p>		
+
+	<?php   endif; ?>	
 	
 </div>
