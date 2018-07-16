26e717120c91633c8b6bfdce27d2c9b174b04c23
julycabigas <july@businessblueprint.com>
2018-07-16 14:58:44 +0800

    update conference

---
+++ page-conference-info.php
@@ -0,0 +1,457 @@
+<?php 
+
+/**
+* Template Name: Conference
+*
+**/
+
+
+get_header(); ?>
+<main id="content" class="content" itemscope itemtype="WebPageElement" itemprop="mainContentOfPage" tabindex="-1">
+	<header class="banner cd-banner" <?php cascade_the_banner_image(); ?>>
+		<div class="container">
+			<h1 class="banner__title">
+				<?php the_title(); ?>
+			</h1>
+
+			<div class="banner__subtitle"></div>
+		</div>
+	</header>
+
+	
+		<!-- end of video-section -->	
+		<div class="cd-section cd-video-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-5 text-center">
+						<h2 class="heading">Business Blueprint FastTrack</h2>
+						<h3 class="subheading">Simple Systems Conference</h3>
+
+						<div class="row">
+							<div class="col-sm-10 col-sm-offset-1">
+								<div class="cd-calendar">
+									<div class="date">
+										<span class="month">Jan</span>
+										<span class="day">19th</span>
+									</div>
+									<div class="date">
+										<span class="month">Jan</span>
+										<span class="day">19th</span>
+									</div>
+									<div class="date">
+										<span class="month">Jan</span>
+										<span class="day">19th</span>
+									</div>
+									<div class="date">
+										<span class="month">Jan</span>
+										<span class="day">19th</span>
+									</div>
+								</div>
+								<div class="cd-button">
+									<div title="" class="btn-addevent addeventatc" id="addeventatc1" aria-haspopup="true" aria-expanded="false" tabindex="0" style="visibility: visible;">
+									    Add to Calendar
+									    <span class="start atc_node">07/19/2018 9:00 am</span>
+									    <span class="end atc_node">07/22/2018 6:00 pm</span>
+									    <span class="timezone atc_node">Pacific/Auckland</span>
+									    <span class="title atc_node">Title</span>
+									    <span class="description atc_node"></span>
+									    <span class="location atc_node"></span>
+										<span class="addeventatc_icon atc_node"></span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>	
+					<div class="col-sm-7">
+						<div class="video">
+<script src="https://fast.wistia.com/embed/medias/vk2k1z2imu.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><span class="wistia_embed wistia_async_vk2k1z2imu popover=true popoverAnimateThumbnail=true popoverContent=link" style="display:inline;position:relative"><a href="#">Preview Video</a></span>
+						</div>
+					</div>
+				</div>
+			</div>	
+		</div>
+		<!-- end of video-section -->
+		<!-- start of nav-section -->
+		<div class="cd-section cd-nav-section">
+			<div class="container">
+				<h3 class="cd-title">Jump to Information...</h3>
+				<div class="row">
+					<div class="col-sm-3">
+						<a href="#date" class="btn">
+							Dates
+						</a>
+					</div>
+					<div class="col-sm-3">
+						<a href="#time" class="btn">
+							Times
+						</a>
+					</div>
+					<div class="col-sm-3">
+						<a href="#venue" class="btn">
+							Venue
+						</a>
+					</div>
+					<div class="col-sm-3">
+						<a href="#contact" class="btn">
+							Contact
+						</a>
+					</div>
+					<div class="col-sm-3">
+						<a href="#date" class="btn">
+							Flights
+						</a>
+					</div>
+					<div class="col-sm-3">
+						<a href="#time" class="btn">
+							Accommodation
+						</a>
+					</div>
+					<div class="col-sm-3">
+						<a href="#venue" class="btn">
+							Parking
+						</a>
+					</div>
+					<div class="col-sm-3">
+						<a href="#contact" class="btn">
+							Activities
+						</a>
+					</div>
+				</div>
+			</div>	
+		</div>
+		<!-- end of nav-section -->
+		
+		<!-- strat of cd-date -->
+		<div class="cd-section cd-date-section">
+			<div class="container">
+				<h3 class="cd-title">Dates</h3>
+				<p class="cd-description">Below are the <strong>confirmed</strong> dates for the conference. Please remember that only Platinum
+members attend all 4 days of the conference, and Gold members attend the middle two
+dates. If you can’t make these dates, don’t worry, as we record all the sessions and upload
+these, along with any slides and handouts, usually within a week.</p>
+
+					<div class="row">
+						<div class="col-sm-10 col-sm-offset-1">
+							<div class="cd-calendar">
+								<div class="date">
+									<span class="month">Jan</span>
+									<span class="day">19th</span>
+								</div>
+								<div class="date">
+									<span class="month">Jan</span>
+									<span class="day">19th</span>
+								</div>
+								<div class="date">
+									<span class="month">Jan</span>
+									<span class="day">19th</span>
+								</div>
+								<div class="date">
+									<span class="month">Jan</span>
+									<span class="day">19th</span>
+								</div>
+							</div>
+							<div class="cd-button">
+								<div title="" class="btn-addevent addeventatc" id="addeventatc1" aria-haspopup="true" aria-expanded="false" tabindex="0" style="visibility: visible;">
+								    Add to Calendar
+								    <span class="start atc_node">07/19/2018 9:00 am</span>
+								    <span class="end atc_node">07/22/2018 6:00 pm</span>
+								    <span class="timezone atc_node">Pacific/Auckland</span>
+								    <span class="title atc_node">Title</span>
+								    <span class="description atc_node"></span>
+								    <span class="location atc_node"></span>
+									<span class="addeventatc_icon atc_node"></span>
+								</div>
+							</div>
+						</div>
+					</div>	
+			</div>
+		</div>
+		<!-- end of cd-date -->
+
+
+		<!-- strat of cod-time -->
+		<div class="cd-section cd-time-section">
+			<div class="container">
+				<h3 class="cd-title">Times</h3>
+				<p class="cd-description">Below are the times for each conference day. Please arrive as promptly as possible,
+allowing time for registration each day, and networking opportunities before, during and
+after each day. It’s our goal to start and finish at these times each day, and we will do our
+best to be on time. If you need to arrive late, leave early or pop out for a bit during the day,
+that is totally fine.</p>
+				<div class="inner-content">
+					
+					<table cellpadding="0" cellspacing="0" class="table table-striped">
+						<thead>
+							<tr>
+								<th></th>
+								<th>Day 1</th>
+								<th>Day 2</th>
+								<th>Day 3</th>
+								<th>Day 4</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td><strong>Registration</strong></td>
+								<td>8:00am - 9:00am</td>
+								<td>8:00am - 9:00am</td>
+								<td>8:00am - 9:00am</td>
+								<td>8:00am - 9:30am</td>
+							</tr>
+							<tr>
+								<td><strong>Event Start</strong></td>
+								<td><strong>9:00am</strong></td>
+								<td><strong>9:00am</strong></td>
+								<td><strong>9:00am</strong></td>
+								<td><strong>9:30am</strong></td>
+							</tr>
+							<tr>
+								<td><strong>Session 1</strong></td>
+								<td>9:00am - 10:45am</td>
+								<td>9:00am - 10:45am</td>
+								<td>9:00am - 10:45am</td>
+								<td>9:00am - 11:30am</td>
+							</tr>
+							<tr>
+								<td><strong>Morning Break</strong></td>
+								<td>10:45am - 11:15am</td>
+								<td>10:45am - 11:15am</td>
+								<td>10:45am - 11:15am</td>
+								<td>11:30am - 12:00pm</td>
+							</tr>
+							<tr>
+								<td><strong>Session 2</strong></td>
+								<td>11:15am - 1:00pm</td>
+								<td>11:15am - 1:00pm</td>
+								<td>11:15am - 1:00pm</td>
+								<td>12:00pm - 1:30pm</td>
+							</tr>
+							<tr>
+								<td><strong>Lunch Break</strong></td>
+								<td>1:00pm - 2:00pm</td>
+								<td>1:00pm - 2:00pm</td>
+								<td>1:00pm - 2:00pm</td>
+								<td>1:30pm - 2:30pm</td>
+							</tr>
+							<tr>
+								<td><strong>Session 3</strong></td>
+								<td>2:00pm - 3:30pm</td>
+								<td>2:00pm - 3:30pm</td>
+								<td>2:00pm - 3:30pm</td>
+								<td>2:30pm - 4:00pm</td>
+							</tr>
+							<tr>
+								<td><strong>Afternoon Break</strong></td>
+								<td>3:30pm - 4:00pm</td>
+								<td>3:30pm - 4:00pm</td>
+								<td>3:30pm - 4:00pm</td>
+								<td>-</td>
+							</tr>
+							<tr>
+								<td><strong>Session 4</strong></td>
+								<td>4:00pm - 5:30pm</td>
+								<td>4:00pm - 5:30pm</td>
+								<td>4:00pm - 5:45pm</td>
+								<td>-</td>
+							</tr>
+							<tr>
+								<td><strong>Event Finish</strong></td>
+								<td><strong>5:30pm</strong></td>
+								<td><strong>5:30pm</strong></td>
+								<td><strong>5:45pm</strong></td>
+								<td><strong>4:00pm</strong></td>
+							</tr>
+							<tr>
+								<td><strong>Social Event</strong></td>
+								<td>-</td>
+								<td><span>(Networking Drinks)</span> <br>6:00pm - 8:00pm</td>
+								<td><span>(Platinum Dinner)</span> <br> 6:00pm - 8:00pm</td>
+								<td>-</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+
+			</div>
+		</div>
+		<!-- end of conference-date -->
+
+		<!-- strat of cd-time -->
+		<div class="cd-section cd-time-section">
+			<div class="container">
+				<h3 class="cd-title">Venue</h3>
+				<p class="cd-description">Our home for the Blueprint members conference is Sydney Masonic Centre. Centrally
+located in the heart of the CBD, and close to hotels, shops, cafes and plenty of great food.</p>
+
+				<div class="inner-content">
+					<div class="venue">
+						<div class="row">
+							<div class="col-sm-6">
+								<img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/smc.jpg" width="100%" />
+							</div>
+							<div class="col-sm-6">
+								<div class="venue-info">
+									<h4>Sydney Masonic Centre</h4>
+									<ul>
+										<li><strong>Address</strong><span>66 Goulburn Street, Sydney</span></li>
+										<li><strong>Room</strong><span>Grand Ballroom </span></li>
+										<li><strong>Map</strong><span>Click Here</span></li>
+										<li><strong>Website</strong><span>Click Here</span></li>
+										<li><strong>Phone</strong><span>(02) 9284 2888 </span></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+		<!-- end of cd-date -->
+
+		<!-- strat of cd-contact-person -->
+		<div class="cd-section cd-contact-person-section">
+			<div class="container">
+				<h3 class="cd-title">Contact</h3>
+				<p class="cd-description">Your contact for the conference is our wonderful General Manager, Emma Lyons. Emma’s
+role during the conference is to ensure that the event runs smoothly and that you have
+everything you need to make your conference experience as enjoyable as possible. You
+can contact Emma for all of your conference related questions on the contact details below.</p>
+
+				<div class="row">
+					<div class="col-sm-12">
+						<img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/emma.jpg" />
+						<ul class="list">
+					 		<li><a href="" class="btn" target="_blank">Voxer</a></li>
+					 		<li><a href="https://web.voxer.com/contact/luke.ahear.voxer.1470707836484.53199165" class="btn" target="_blank">SMS</a></li>
+					 		<li><a href="https://calendly.com/julycabigas/15min" class="btn" target="_blank">Email</a></li>
+					 		<li><a href="https://www.linkedin.com/in/hearnsy/" class="btn" target="_blank">Phone</a></li>
+					 	</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- end of cd-contact-person -->
+
+		<!-- strat of cd-contact-person -->
+		<div class="cd-section cd-flights-section">
+			<div class="container">
+				<h3 class="cd-title">Flights</h3>
+				<p class="cd-description">If you’re travelling from interstate, you will need to arrange your flights to Sydney. Most
+guests arrive the night before their first day of conference as to avoid traffic from the airport
+on the morning of the event. And then return travel is up to you, either the night of your last
+day or the next day. Below are some options to help you organise your flights.</p>
+
+				<ul>
+					<li><a href="">Webjet</a></li>
+					<li><a href="">Skyscanner</a></li>
+					<li><a href="">Last Minute</a></li>
+				</ul>				
+			</div>
+		</div>
+		<!-- end of cd-contact-person -->
+
+		<!-- strat of cd-accommodation -->
+		<div class="cd-section cd-accommodation-section">
+			<div class="container">
+				<h3 class="cd-title">Accommodation</h3>
+				<p class="cd-description">There are plenty of accommodation options nearby, it just depends on your budget and
+level of comfort you require when travelling. We’ve teamed up with Airbnb to easily give you
+the ability to book for the dates below. We’ve also provided some nearby hotel and
+apartment options should you prefer these.</p>
+
+				<div class="row">
+					<div class="col-sm-12">
+						<div class="embed-content">
+							<div class='airbnb-embed-frame' data-id=167678 data-view='event' data-height=500 data-width=1000 style='margin:auto;height: 450px;'><a href='https://www.airbnb.com/s/66 Goulburn St, Sydney NSW 2000?event_id=167678'><img src='https://a0.muscache.com/pictures/1ef886e1-5915-4428-bcc7-23b1dba1291e.jpg' style='display:block; margin:auto'></a><script async src='https://www.airbnb.com/embeddable/airbnb_jssdk'></script></div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+		<!-- end of cd-accommodation -->
+
+		<!-- strat of cd-parking -->
+		<div class="cd-section cd-parking-section">
+			<div class="container">
+				<h3 class="cd-title">Parking</h3>
+				<p class="cd-description">If you are driving to the venue, there are multiple parking options available. Some of the
+conference days are weekdays, so parking will be a little bit more expensive, however, there
+are early bird parking rates available. On the weekend, there are flat rate parking options
+nearby. You might also require parking in other cities, and we have a deal with a fellow
+Blueprint member for Melbourne Airport parking. All details are below.</p>
+
+				<div class="inner-content">
+					<ul class="list-inline">
+						<li><a href=""><span>www.carparking.info</span></a></li>
+						<li><a href=""><span>Melbourne Parking</span></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<!-- end of cd-parking -->
+
+		<!-- strat of cd-social-->
+		<div class="cd-section cd-social-section">
+			<div class="container">
+				<h3 class="cd-title">Social Activities</h3>
+				<p class="cd-description">Each night we hold a different social activity, however, please note that these aren’t open to
+everyone. Below is all the information you need about the social activities, who they are for,
+and how you can RSVP..</p>
+				
+				<div class="inner-content">
+					<div class="cd-box-grid">
+						<div class="activity-item">
+							<div class="inner">
+								<h3>Day 1</h3>
+								<h4>First Sign Ups Dinner</h4>
+								<ul>
+									<li><strong>Who</strong><span>First 7 people to join BB last tour. You will be contacted.</span></li>
+									<li><strong>Where</strong><span>Criniti's Darling Harbour</span></li>
+									<li><strong>When</strong><span>7:30pm - 9:30pm</span></li>
+								</ul>
+								<a href="">RSVP</a>
+							</div>
+						</div>
+						<div class="activity-item">
+							<div class="inner">
+								<h3>Day 1</h3>
+								<h4>First Sign Ups Dinner</h4>
+								<ul>
+									<li><strong>Who</strong><span>First 7 people to join BB last tour. You will be contacted.</span></li>
+									<li><strong>Where</strong><span>Criniti's Darling Harbour</span></li>
+									<li><strong>When</strong><span>7:30pm - 9:30pm</span></li>
+								</ul>
+								<a href="">RSVP</a>
+							</div>
+						</div>
+						<div class="activity-item">
+							<div class="inner">
+								<h3>Day 1</h3>
+								<h4>First Sign Ups Dinner</h4>
+								<ul>
+									<li><strong>Who</strong><span>First 7 people to join BB last tour. You will be contacted.</span></li>
+									<li><strong>Where</strong><span>Criniti's Darling Harbour</span></li>
+									<li><strong>When</strong><span>7:30pm - 9:30pm</span></li>
+								</ul>
+								<a href="">RSVP</a>
+							</div>
+						</div>
+					</div>
+			
+				</div>
+
+			</div>
+		</div>
+		<!-- end of cd-social -->
+
+
+	</div>
+
+</main>
+<!-- AddEvent script -->
+<script type="text/javascript" src="https://addevent.com/libs/atc/1.6.1/atc.min.js" async defer></script>
+<?php
+
+get_footer();
