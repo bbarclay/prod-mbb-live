2bbc62e33fd66ede1edb6d360ac406a00f0fd35d
julycabigas <july@businessblueprint.com>
2018-09-11 10:51:12 +0800

    added guest_registration.php

---
+++ template-parts/conference/guest_registration.php
@@ -0,0 +1,11 @@
+<div class="cd-section cd-registration-section" id="registration">
+	<div class="container">
+		<?php if( get_sub_field('heading') ) : ?>
+			<h3 class="cd-title"><?php echo get_sub_field('heading') ?></h3>
+		<?php endif;  ?>
+		<?php if( get_sub_field('description') ) : ?>	
+			<div class="cd-description"><?php echo get_sub_field('description') ?></div>
+		<?php endif;  ?>
+
+	</div>
+</div>
