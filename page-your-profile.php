e818fc629a80a60f130986d913f1acdbec4362be
julycabigas <july@businessblueprint.com>
2018-09-12 13:48:56 +0800

    update profile page

--- page-your-profile.php
+++ page-your-profile.php
@@ -9,7 +9,14 @@ get_header();
 
 	<section class="module module__membership">
 		<div class="container">
-			<div class="notes" id="bb-note"><span class="fa fa-exclamation-circle"></span><span class="text"> IMPORTANT: All Members (Fast Track & Elite) can view your name and state below, you can choose what other information to share or hide by clicking the edit button.</span></div> 
+		<?php 
+
+			$is_silver =  do_shortcode('[mbb_silver_membership]');
+
+			if(!$is_silver) :
+		?>
+				<div class="notes" id="bb-note"><span class="fa fa-exclamation-circle"></span><span class="text"> IMPORTANT: All Members (Fast Track & Elite) can view your name and state below, you can choose what other information to share or hide by clicking the edit button.</span></div> 
+		<?php endif; ?>	
 			<?php echo do_shortcode('[MBB_contactInfo]') ?>	
 		</div>			
 	</section>
