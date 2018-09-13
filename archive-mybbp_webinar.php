c13793d33d2ddfa3974764ec3d3c804de97acc72
julycabigas <july@businessblueprint.com>
2018-09-13 15:06:10 +0800

    update archive webinar

--- archive-mybbp_webinar.php
+++ archive-mybbp_webinar.php
@@ -3,8 +3,16 @@
 	<main id="content" class="content" itemscope itemtype="WebPageElement" itemprop="mainContentOfPage" tabindex="-1">
 		<?php get_template_part( 'template-parts/content/header' ); ?>
 
-		<div class="container">       
-            <a href="http://my.businessblueprint.com/upcoming-webinars/" class="button button-custom">Upcoming Webinars</a>
+		<div class="container"> 
+           <?php 
+
+            $is_silver =  do_shortcode('[mbb_silver_membership]');
+
+            if(!$is_silver) :  ?>      
+                <a href="http://my.businessblueprint.com/upcoming-webinars/" class="button button-custom">Upcoming Support Webinars</a>
+            <?php else: ?>
+                <a href="http://my.businessblueprint.com/upcoming-webinars/" class="button button-custom">Upcoming Webinars</a>
+            <?php endif; ?>
 			<?php
             if ( have_posts() ) : $prev_year = 0; $i = 0;
                 while ( have_posts() ) : the_post(); $year = get_the_time( 'Y' );
