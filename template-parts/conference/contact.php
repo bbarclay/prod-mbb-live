baee15329ce34270425989ce1a3c474abea6fd0e
Irvin Garcia <irvin.garcia@brin.ai>
2019-05-21 15:58:26 +1000

    test commit

--- template-parts/conference/contact.php
+++ template-parts/conference/contact.php
@@ -10,7 +10,7 @@
 					<div class="info">
 						<div class="info-left">
 							<?php echo wp_get_attachment_image( get_sub_field('image'), 'full' ) ?>
-							<div class="name" id="">General Manager</div>
+							<div class="name">General Manager</div>
 						</div>
 						<div class="bio info-right">
 							<?php echo get_sub_field('description') ?>
